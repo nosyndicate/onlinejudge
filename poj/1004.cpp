4e45550210fb2a35341782dd955a01a9a93ceb39
Ermo Wei <nosyndicate@gmail.com>
2012-06-15 13:55:51 -0400

    add 1004

---
+++ poj/1004.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int t = 12;
+    double sum = 0, single;
+    while(t--)
+    {
+	cin >> single;
+	sum+=single;	
+    }
+    cout << "$" << sum/12 <<endl;
+    return 0;    
+}
